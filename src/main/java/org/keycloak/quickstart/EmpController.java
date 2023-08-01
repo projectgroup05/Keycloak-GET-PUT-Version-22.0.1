bb7fb4a23abed4ef20cb78dfd58fd32756dda650
Shriprasad Gaikwad <shriprasad.gaikwad@avisys.in>
2023-08-01 15:53:51 +0530

    keycloak commit

---
+++ src/main/java/org/keycloak/quickstart/EmpController.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2002-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.keycloak.quickstart;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("employee")
+public class EmpController {
+
+	@GetMapping("/get")
+	public String getAllDesignation() {
+		return "Hello from Test Controller";
+	}
+
+	@PostMapping("/post")
+	public String create() {
+		return "Test Controller - Create";
+	}
+
+	@DeleteMapping("/delete")
+	public String getDesignationById() {
+		return "Test Controller - deleteById";
+	}
+
+	@GetMapping("/get-all")
+	public String index(@AuthenticationPrincipal Jwt jwt) {
+		return String.format("Emp : Hello, %s!", jwt.getClaimAsString("preferred_username"));
+	}
+
+	@GetMapping("/protected/premium")
+	public String premium(@AuthenticationPrincipal Jwt jwt) {
+		return String.format("Hello, %s!", jwt.getClaimAsString("preferred_username"));
+	}
+}
